bd064a59ada30fa4ee0fd37d1c26f5ac817dbf94
Hamad <155316963+HamadCodes@users.noreply.github.com>
2024-03-30 04:01:57 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inqirer from "inquirer";
+
+let list = []
+let isrunning =true
+while(isrunning){
+const task = await inqirer.prompt([
+    { message: "What task do you want to add to the list?", type: "input", name: "tsk" },
+]);
+
+list.push(task.tsk)
+
+console.log(list)
+
+const again = await inqirer.prompt([
+    { message: "Do you want to add more tasks to the list?", type: "confirm", name: "confirm" },
+]);
+
+if(!again.confirm){
+    isrunning = false
+}
+
+}
